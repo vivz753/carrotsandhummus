d2476f55fb5c054c93f49a04b4cedc913d6b3e3e
Vivian Leung <vivz753@gmail.com>
2023-06-14 13:15:58 -0700

    mobile adjustments for FAQ, fix z-indexes cuz sidecart was under searchbar

--- web/components/Layout/Navbar.tsx
+++ web/components/Layout/Navbar.tsx
@@ -8,7 +8,7 @@ const navLinks = ["/", "/about"]
 
 export const Navbar: FC<{ setSideCart: Dispatch<SetStateAction<boolean>> }> = ({ setSideCart }) => {
   return (
-    <div className="absolute top-0 z-10 flex h-20 max-h-20 w-full flex-row items-center justify-start gap-5 bg-p5 px-20">
+    <div className="absolute top-0 z-20 flex h-20 max-h-20 w-full flex-row items-center justify-start gap-5 bg-p5 px-20">
       {navLabels.map((label, i) => (
         <Link href={navLinks[i]} key={i} className="text-bold font-title text-lg capitalize text-white">
           {label}
--- web/pages/faq.tsx
+++ web/pages/faq.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next"
 const FAQ: NextPage = () => {
   return (
     <div className="flex h-full w-screen flex-col items-center justify-center pt-20">
-      <div className="my-20 flex w-4/5 flex-col gap-10 rounded-xl border-4 border-p5 p-20 lg:w-1/2">
+      <div className="my-20 flex w-11/12 flex-col gap-10 rounded-xl border-4 border-p5 p-8 lg:w-1/2 lg:p-20">
         <span className="text-3xl">FAQ</span>
         <div className="flex flex-col gap-5">
           <span className="text-xl">When will my item(s) arrive???</span>
