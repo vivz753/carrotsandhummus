f441efa8a23f9606dc101d0a9f0ea48b9deec3d6
Vivian Leung <vivz753@gmail.com>
2025-01-09 18:14:05 -0800

    SideCart: try to fix the image[0] issue

--- web/components/Layout/SideCart.tsx
+++ web/components/Layout/SideCart.tsx
@@ -56,7 +56,7 @@ export const SideCart: FC<{ view: boolean; setView: Dispatch<SetStateAction<bool
                     name={data.name}
                     category={data.category}
                     quantity={data.quantity}
-                    image={data.images[0] ?? ""}
+                    image={data.images && data.images[0] ? data.images[0] : ""}
                     price={data.price}
                   />
                 ))}
